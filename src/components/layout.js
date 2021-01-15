98dddad7bfbb61c7e867397d1a822d45c08782f8
dev-empire <alaminyusuf131@gmail.com>
2021-01-15 22:19:44 +0100

    Rocket Lauch

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,39 +7,18 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div style={{ backgroundColor: "#f8f8f8", color: "#000" }}>
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
+        <footer style={{ textAlign: "center" }}>
+          © {new Date().getFullYear()}
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.github.com/dev-empire">Audi Nation</a>
         </footer>
       </div>
     </>
