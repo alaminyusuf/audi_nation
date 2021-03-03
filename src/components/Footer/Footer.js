f9e558ecb69d9afefcc9cc717411b81773cc4884
dev-empire <alaminyusuf131@gmail.com>
2021-03-03 16:57:31 +0100

    finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,13 @@ import { Link } from "gatsby"
 import tw from "twin.macro"
 
 const Wrapper = tw.div`bg-primary`
-const Body = tw.footer`py-10 grid grid-cols-1 md:grid-cols-2`
+const Body = tw.footer`pt-10 pb-5 grid grid-cols-1 md:grid-cols-2`
 const NewsLetter = tw.input`w-3/4 bg-primary text-white px-3`
 const Label = tw.label`uppercase block text-secondary py-2 text-left`
 const Text = tw.p`text-white uppercase mt-3`
 const Btn = tw.button`bg-secondary text-white uppercase p-1 ml-3`
 const Form = tw.form``
-const ImageContainer = tw.div`h-8 w-24 bg-gray-500 mx-auto md:my-auto`
+const ImageContainer = tw.div`h-8 w-24 bg-gray-500 mx-auto md:my-auto mt-2`
 
 const Footer = () => {
   function handleSubmit(e) {
@@ -28,6 +28,7 @@ const Footer = () => {
           </Form>
           <Text>Email: info@audination.com</Text>
           <Text>Tel: +2342700383882</Text>
+          <Text>Plot 321, Ayoba</Text>
         </div>
         <div>
           <Text>
