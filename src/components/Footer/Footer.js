b708847cf10988e0138b5f7d38f2e27a2d6c4723
dev-empire <alaminyusuf131@gmail.com>
2021-03-03 16:52:51 +0100

    finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,10 @@ const Wrapper = tw.div`bg-primary`
 const Body = tw.footer`py-10 grid grid-cols-1 md:grid-cols-2`
 const NewsLetter = tw.input`w-3/4 bg-primary text-white px-3`
 const Label = tw.label`uppercase block text-secondary py-2 text-left`
-const Text = tw.p`text-white uppercase`
-const Email = tw.div`text-white uppercase mt-5`
+const Text = tw.p`text-white uppercase mt-3`
 const Btn = tw.button`bg-secondary text-white uppercase p-1 ml-3`
 const Form = tw.form``
+const ImageContainer = tw.div`h-8 w-24 bg-gray-500 mx-auto md:my-auto`
 
 const Footer = () => {
   function handleSubmit(e) {
@@ -26,12 +26,16 @@ const Footer = () => {
               Submit
             </Btn>
           </Form>
-          <Email>Email: info@audination.com</Email>
+          <Text>Email: info@audination.com</Text>
+          <Text>Tel: +2342700383882</Text>
         </div>
         <div>
           <Text>
             <Link href="http://localhost:8000">Audi Nation</Link>
           </Text>
+          <Text>Lagos, nigeria</Text>
+          <Text>&copy; 2021 All rights reserved</Text>
+          <ImageContainer></ImageContainer>
         </div>
       </Body>
     </Wrapper>
